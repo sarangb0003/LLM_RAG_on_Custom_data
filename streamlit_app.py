7bf364aa43dbf837139c618277c505fe01b99ba2
Sarang Bagul <61322867+sarangb0003@users.noreply.github.com>
2023-09-16 11:14:02 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,21 @@
 import streamlit as st
 from streamlit_chat import message
-# from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
-# from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 
 from langchain import HuggingFaceHub
 from langchain.chains import ConversationalRetrievalChain,RetrievalQA
-# from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.document_loaders import PyPDFLoader,TextLoader
-# from langchain.vectorstores import FAISS
-#from langchain.vectorstores import DocArrayInMemorySearch
-#from langchain.vectorstores import Chroma
-from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
 import tempfile
 
-from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from chromadb.errors import InvalidDimensionException
+
 repo_id = "google/flan-t5-xxl"
 
 # App title
-st.set_page_config(page_title="LLM Chatbot")
-
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
     placeholder="Paste your openAI API key, sk-",
@@ -29,7 +23,6 @@ user_api_key = st.sidebar.text_input(
 
 uploaded_file = st.sidebar.file_uploader("upload", type="pdf")
 
-# Hugging Face Credentials
 if uploaded_file :
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(uploaded_file.getvalue())
@@ -37,55 +30,56 @@ if uploaded_file :
 
     loader = PyPDFLoader(file_path=tmp_file_path)
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=900, chunk_overlap=150)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     docs = text_splitter.split_documents(documents)
     
-
-    embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=user_api_key)
-    vectors = FAISS.from_documents(docs, embeddings)
+    embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=user_api_key )
     
-    retriever = vectors.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+    try:
+        vectors = Chroma.from_documents(documents=docs, embedding=embeddings)
+    except InvalidDimensionException:
+        Chroma().delete_collection()
+        vectors = Chroma.from_documents(documents=docs, embedding=embeddings)
     
+    retriever = vectors.as_retriever()
+    
+    llm = HuggingFaceHub(huggingfacehub_api_token=user_api_key, repo_id=repo_id, model_kwargs={"temperature": 0.1})
+    
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-    chain = ConversationalRetrievalChain.from_llm(llm = HuggingFaceHub(huggingfacehub_api_token=user_api_key,
-                                                                       repo_id=repo_id, retriever=retriever),
-                                                                      return_source_documents=True,
-                                                                        return_generated_question=True,model_kwargs={"temperature": 0.1})
+    chain = ConversationalRetrievalChain.from_llm(llm = llm, retriever=retriever, memory=memory)
+    
+    
     def conversational_chat(query):
         
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
+        result = chain({"question": query})#, "chat_history": st.session_state['history']})
+#         st.session_state['history'].append((query, result["answer"]))
         
         return result["answer"]
     
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+#     if 'history' not in st.session_state:
+#         st.session_state['history'] = []
 
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "Welcome to HuggingFace Chatbot Assistant 🤗. How can I help you ?"}]
+    
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey ! 👋"]
-        
-    #container for the chat history
-    response_container = st.container()
-    #container for the user's text input
-    container = st.container()
 
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            
-            user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-            
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-            
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+    # User-provided prompt
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = conversational_chat(prompt) 
+                st.write(response) 
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message) 
